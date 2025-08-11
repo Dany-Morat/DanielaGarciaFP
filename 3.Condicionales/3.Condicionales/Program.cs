9752f647c262e38fef2637361fe72f157cde54f4
Dany Morat <daniela.garciamo@upb.edu.co>
2025-08-11 13:13:41 -0500

    Condicional simple

---
+++ 3.Condicionales/3.Condicionales/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _3.Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONDICIONAL SIMPLE
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre = Console.ReadLine();//todo lo q yo leo desde la consola llega aca como string
+            Console.WriteLine(nombre);
+            Console.WriteLine("Ingrese sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+            if (sueldo > 3000) 
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            
+            }
+        
+        }
+    }
+}
