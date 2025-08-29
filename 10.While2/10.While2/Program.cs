dd8730b2294dd7b87d4fde7b15030f40c444cb80
Dany Morat <daniela.garciamo@upb.edu.co>
2025-08-29 15:32:45 -0500

    Add files via upload

---
+++ 10.While2/10.While2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _10.While2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita calcular la factorial de un numero dado por el usuario (5!=1*2*3*4*5)
+            int acumulador = 1;
+            int contador = 1;
+            int num = 0;
+
+            Console.WriteLine("Ingrese un numero para calcular su factorial:");
+            num=int.Parse(Console.ReadLine());
+             
+            while (contador<=num)
+            {
+                acumulador=acumulador*contador;
+                Console.WriteLine($"Contador: {contador} - Factorial: { acumulador}");
+                contador++;
+
+            }
+            Console.WriteLine($"El factorial es numero {num}, es: {acumulador}");
+        }
+    }
+}
