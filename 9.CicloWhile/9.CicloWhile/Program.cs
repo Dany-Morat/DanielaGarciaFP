b6ae0e2a646984625070e83e9f24d9977788c3aa
Dany Morat <daniela.garciamo@upb.edu.co>
2025-08-29 15:30:26 -0500

    Add files via upload

---
+++ 9.CicloWhile/9.CicloWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _9.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int acumulador = 0;
+
+            while (contador <=5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"C: {contador}-A: {acumulador}");
+                contador++; 
+            }
+            Console.WriteLine($"La suma de los cinco primeros numeros eneteros es: {acumulador}");
+
+
+        }
+    }
+}
