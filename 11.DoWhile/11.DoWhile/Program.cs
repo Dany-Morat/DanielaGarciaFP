ab451bce30b4dd49aeb6c8349019623725b3eb0d
Dany Morat <daniela.garciamo@upb.edu.co>
2025-09-05 15:32:56 -0500

    Add files via upload

---
+++ 11.DoWhile/11.DoWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _11.DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //algoritmo que genera y suma los primeros 5 numeros enteros
+
+            /*int contador = 0;
+            int acumulador = 0;
+
+            do
+            {
+                contador++;
+                acumulador = acumulador + contador;
+            } while (contador < 5);
+
+            Console.WriteLine($"La suma de los 5 primeros numeros enteros es: {acumulador} ");*/
+
+            // algoritmo que solicite un numero y genere su tabla de multiplicar desde el 1 hasta el 10
+            // y asi sucesivamente hasta que el usuario ya no desee continuar generando tablas de multiplicar
+            
+            
+
+        }
+    }
+}
